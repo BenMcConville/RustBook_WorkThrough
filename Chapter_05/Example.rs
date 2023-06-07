b2c81dc2c55791b6a3654c4594c14377ad82957f
BMcC59 <s2221528@ed.ac.uk>
2023-06-07 10:57:03 -0600

    Finished Chapter_05

--- Chapter_05/Example.rs
+++ Chapter_05/Example.rs
@@ -11,8 +11,11 @@ fn main()	{
 		length: 50,
 		width:	20
 	};
+	let rec_02 = Rec { length: 40, width: 10 };
+	let _rec_03 = Rec { length: 45, width: 60 };
 	println!("Rect_01 is {:#?}", rec_01); // can use {:?}
-	println!("Area of Rec is {}", area(&rec_01));
+	//println!("Area of Rec is {}", area(&rec_01)); This used with func
+	println!("Rec 01 holds Rec 02 {}", rec_01.can_hold(&rec_02));
 }
 /* We can always create functions but to limit which types can call these functions we implement methods
 fn area(rectangle: &Rec) -> u32	{
@@ -21,5 +24,8 @@ fn area(rectangle: &Rec) -> u32	{
 impl Rec	{
 	fn area(&self) -> u32	{
 		self.length * self.width
+	}
+	fn can_hold(&self, r: &Rec) -> bool	{
+		self.area() > r.area()
 	}
 }
