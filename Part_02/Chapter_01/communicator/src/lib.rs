dd505d74131f0bd6f9ca3174eed30e7246596d3b
BMcC59 <s2221528@ed.ac.uk>
2023-06-08 21:12:51 -0600

    Part_02 Chapter01

---
+++ Part_02/Chapter_01/communicator/src/lib.rs
@@ -0,0 +1,36 @@
+/*
+src/lib.rs has this module hierarchy:
+
+communicator
+	client
+	network
+		server
+
+*/
+
+mod network	{
+	fn connect()	{
+
+	}
+
+	mod server	{
+		fn connect()	{
+
+		}
+	}
+}
+
+mod client	{
+	fn connect()	{
+
+	}
+{
+
+
+#[cfg(test)]
+mod tests	{
+	#[test]
+	fn it_works()	{
+	
+	}
+}
